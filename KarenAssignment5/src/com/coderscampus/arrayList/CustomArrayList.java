507149889ca47bc42ff7036fea93be05c6024045
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2023-03-21 15:20:25 -0500

    Add files via upload
    hopefully last version of Assignment 5.

--- KarenAssignment5/src/com/coderscampus/arrayList/CustomArrayList.java
+++ KarenAssignment5/src/com/coderscampus/arrayList/CustomArrayList.java
@@ -1,61 +1,45 @@
 package com.coderscampus.arrayList;
 
+import java.util.Arrays;
 
 public class CustomArrayList<T> implements CustomList<T> {
-	
+
 	Object[] items = new Object[10];
+	int size = 0;
+	int b = 0;
+
 	@Override
 	public boolean add(T item) {
-		int nextInt = 0;
-		for (int i =0; i<items.length; i++) {
-			if (items[i] ==null) {
-				nextInt = i;
-				break;
-			}
-			// check to see if array is full, if it is, double the array with "*2
-			// then copy over everything to new array
-			if(nextInt == items.length -1) {
-				Object [] newItems = new Object[items.length * 2];
-				for (int n = 0; n <items.length; n++) {
-					newItems[n] = items[n];
-				}
-				//ensure all array item are carried over and check to see if they were actually added
-				items = newItems;
-			}
-			items[nextInt]= item;
-		}if (nextInt < items.length) {
-			items[nextInt] = item;
-			return true;
-			
-			//have sysout for if items were added correctly or not
-		}
-		System.out.println("added sucessfully");
-		return false;
+		items[b] = item;
+		b++;
+		if (item == null )
+			return false;
+		System.out.println( item + " :added sucessfully");
+
+		Object[] newItems = Arrays.copyOf(items, items.length * 2);
+		items = newItems;
+
+		return true;
 	}
-	
-	
-
-	
-	
-
-//	@Override
-//	public boolean add(T item) {
-		
-
-//
-//		return false;
-//	}
 
 	@Override
 	public int getSize() {
+		int count = 0;
 
-		return 0;
+		for (int i = 0; i < items.length; i++) {
+			if (items[i] != null) {
+				count++;
+			}
+		}
+		return count;
 	}
 
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) {
 
-		return null;
-	}
+		return (T) items[index];
 
+	}
 }
